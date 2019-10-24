17083978516d03f186d7a5a43b8a1b24b96fbdfc
Ori Mansur <orimansur@gmail.com>
2019-10-24 20:05:21 +0300

    meme gen 003

--- js/main.js
+++ js/main.js
@@ -2,18 +2,49 @@
 
 let gCanvas;
 let gCtx;
-let gCurrTxt=1
-gCanvas = document.querySelector('#meme-edotir');
-    gCtx = gCanvas.getContext('2d')
-    resizeCanvas()
-function onOpenCanvas(el){
+
+function init(){
+
+    renderImgs()
+}
+
+
+function onOpenCanvas(el) {
     toggelHide('.gallery')
     toggelHide('.keywords')
     toggelHide('.canvas-container')
+    toggelHide('.editor-tools')
     gCanvas = document.querySelector('#meme-edotir');
     gCtx = gCanvas.getContext('2d')
     resizeCanvas()
+    renderStickers()
     drawImg(el)
+    renderCanvas()
+}
+function renderImgs() {
+    var imgs = getImgs()
+    var HTMLs = ''
+    imgs.forEach(img => {
+        HTMLs += `<img src="${img.url}" id="${img.id}" onclick="onOpenCanvas(this)"></img>`
+
+    })
+    document.querySelector('.gallery').innerHTML = HTMLs
+}
+
+function renderStickers() {
+    var stickers = getStickers()
+    var HTMLs = `<a class="prev" onclick="plusSlides(-1)">&#10094;</a>`
+    stickers.stickers.forEach(sticker => {
+        HTMLs += `<img src="${sticker.url}" class="png" id="${sticker.id}" ondrag="onSelectSticker(this)">`
+    })
+    HTMLs += `<a class="next" onclick="plusSlides(1)">&#10095;</a>`
+    document.querySelector('.stickers').innerHTML = HTMLs
+
+}
+function moveTxt(ev) {
+    var objtxt = getTxt()
+    var x = gCtx.measureText(objtxt.line)
+    if (ev.offsetX > x.actualBoundingBoxLeft) console.log(x, ev.offsetX);
 
 }
 
@@ -23,75 +54,120 @@ function resizeCanvas() {
     gCanvas.width = elContainer.offsetWidth
     gCanvas.height = elContainer.offsetHeight
 }
-function onDrawTxt(elVal){
+function onDrawTxt(elVal) {
     editTxt(elVal)
-    defineTxtSetting()
-    
+    renderCanvas()
+
     // document.querySelector('.meme').value=''
 }
-function defineTxtSetting(){
-    var txt=getTxt(0)
-    var y=txt.size+5
-    gCtx.clearRect(0,0,gCanvas.width,y+5)
-    drawText(txt)
+function renderCanvas() {
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
+    var memes = getMemes()
+    var img = document.getElementById(memes.selectedImgId)
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    memes.txts.forEach((txt) => {
+        drawText(txt)
+    })
 }
 
 function drawImg(elImg) {
+    updateSelectedImg(elImg)
     const img = elImg
     gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
 }
 
 function drawText(txt) {
-    gCtx.textAlign=txt.align
-    var y=txt.size
+    gCtx.textAlign = txt.align
     gCtx.fillStyle = txt.color
-    gCtx.font = txt.size+'px Arial';
-    gCtx.fillText(txt.line, gCanvas.width/2, y);
-    // gCtx.strokeText(txt.line, x, y);
-    
+    gCtx.strokeStyle = txt.frameColor
+    gCtx.font = txt.size + 'px ' + txt.font;
+    gCtx.fillText(txt.line, gCanvas.width / 2, txt.y);
+    gCtx.strokeText(txt.line, gCanvas.width / 2, txt.y);
+
+
 }
 
-function onEditTxtColor(color){
-    
+function onEditTxtColor(color) {
+
     editTxtColor(color)
-    defineTxtSetting()  
+    renderCanvas()
 }
-function onEditTxt(txt){
+function onEditTxtFillColor(color) {
+    editTxtFillColor(color)
+    renderCanvas()
+}
+function onEditTxt(txt) {
     editTxt(txt)
-}            
-function onEditTxtSize(operator){
+}
+function onEditTxtSize(operator) {
     editTxtSize(operator)
-    defineTxtSetting()
-   
-}            
-function onEditTxtAlign(txtAlign){
-    editTxtAlign(txtAlign) 
-    defineTxtSetting()  
-} 
+    renderCanvas()
 
-function onRemoveTxt(){
+}
+function onMoveLine(operator) {
+    moveLine(operator)
+    renderCanvas()
+
+}
+
+function onEditTxtAlign(txtAlign) {
+    editTxtAlign(txtAlign)
+    renderCanvas()
+}
+function onChangeFont(font) {
+    changeFont(font)
+    renderCanvas()
+}
+
+function onRemoveTxt() {
     removeTxt()
-    gCtx.clearRect(0,0,gCanvas.width,40)
+    renderCanvas()
+
 }
-function onAddTxt(){
+function onAddTxt() {
+    document.querySelector('.meme').value = ''
     addTxt()
+    renderCanvas()
 }
-function onChangeCurrTxt(){
+function onChangeCurrTxt() {
     changeCurrTxt()
+    var objtxt = getTxt()
+    var width = gCtx.measureText(objtxt.line).width + 15;
+    var x = gCanvas.width / 2 - width / 2
+    gCtx.strokeStyle = 'gray'
+    gCtx.strokeRect(x, objtxt.y + 5 - objtxt.size, width, objtxt.size + 5)
+    setTimeout(function () {
+        renderCanvas()
+    }, 3000)
 }
-function setAlignTxt(txt){
+function onSelectSticker(el) {
+    selectSticker(el)
+}
+function onSetIconPos(ev) {
+    ev.preventDefault()
+    var stickers = getStickers()
+    var img = document.getElementById(stickers.selectedStickerId)
+    console.log(ev.view, gCanvas.height, img.style.width);
+    gCtx.drawImage(img, 0, 0, 40, 40)
+}
+function setAlignTxt(txt) {
     var x;
-    gCtx.txtAlign=txt.align
-switch(txt.align) {
-    case 'center':  
-        x = gCanvas.width / 2
-      break;
-    case 'left':
-     x = 10
-      break; 
-    case 'right':
-      x = gCanvas.width-(txt.line.length*txt.size);
-      break;
-  }
-  return x
+    gCtx.txtAlign = txt.align
+    switch (txt.align) {
+        case 'center':
+            x = gCanvas.width / 2
+            break;
+        case 'left':
+            x = 10
+            break;
+        case 'right':
+            x = gCanvas.width - (txt.line.length * txt.size);
+            break;
+    }
+    return x
+}
+
+function toggleMenu() {
+    var nav = document.querySelector('nav')
+    nav.classList.toggle('open-nav');
 }
--- js/service.js
+++ js/service.js
@@ -1,52 +1,116 @@
 'use strict'
 
 
-var gKeywords = {'happy': 12,'funny puk': 1} 
+var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gImgId = 1
+var gStickerId = 101
+var gImgs = [{ id: gImgId++, url: 'imgs/003.jpg', keywords: ['happy'] },
+{ id: gImgId++, url: 'imgs/004.jpg', keywords: ['happy'] },
+{ id: gImgId++, url: 'imgs/005.jpg', keywords: ['happy'] },
+{ id: gImgId++, url: 'imgs/006.jpg', keywords: ['happy'] },
+{ id: gImgId++, url: 'imgs/12.jpg', keywords: ['happy'] },
+{ id: gImgId++, url: 'imgs/19.jpg', keywords: ['happy'] },
+{ id: gImgId++, url: 'imgs/5.jpg', keywords: ['happy'] },
+{ id: gImgId++, url: 'imgs/8.jpg', keywords: ['happy'] },
+{ id: gImgId++, url: 'imgs/9.jpg', keywords: ['happy'] }
+];
 
-var gImgs = [{id: 1, url: 'img/popo.jpg', keywords: ['happy']}]; 
 
-var gMeme = { selectedImgId: 5, 
-              selectedTxtIdx: 0, 
-              txts: [ { line: 'I never eat Falafel', 
-                        size: 20, 
-                        align: 'right', 
-                        color: 'red' } 
-                    ]
-            }
-function getTxt(selectedTxt){
-    return gMeme.txts[selectedTxt]
+var gMeme = {
+    selectedImgId: 5,
+    selectedTxtIdx: 0,
+    txts: [{
+        line: 'I never eat Falafel',
+        size: 28,
+        align: 'center',
+        color: 'white',
+        frameColor: 'black',
+        font: 'Impact',
+        y: 25,
+        draggable: true
+    }
+    ]
 }
 
-function editTxt(txt){
-    gMeme.txts[gMeme.selectedTxtIdx].line=txt
-}            
-function editTxtSize(operator){
-    if(gMeme.txts[gMeme.selectedTxtIdx].size<3 && operator<0)return
-    gMeme.txts[gMeme.selectedTxtIdx].size+=operator
-}            
-function editTxtAlign(txtAlign){
-    gMeme.txts[gMeme.selectedTxtIdx].align=txtAlign
-    console.log(gMeme.txts[gMeme.selectedTxtIdx].align);
-    
-}            
-function editTxtColor(color){
-    gMeme.txts[gMeme.selectedTxtIdx].color=color
-} 
-function removeTxt(){
-    gMeme.txts.splice(gMeme.selectedTxtIdx,1)
+var gStickers = {
+    selectedStickerId: 0,
+    stickers: [
+        { id: gStickerId++, url: 'png-icons/001-attachment.png' },
+        { id: gStickerId++, url: 'png-icons/002-healthcare-and-medical.png' },
+        { id: gStickerId++, url: 'png-icons/007-gift.png' },
+        { id: gStickerId++, url: 'png-icons/008-donation.png' }
+    ]
+
+}
+function getMemes() {
+    return gMeme
+}
+function getTxt() {
+    return gMeme.txts[gMeme.selectedTxtIdx]
+}
+function getImgs() {
+    return gImgs
+}
+function getStickers() {
+    return gStickers
+}
+
+function selectSticker(el) {
+    gStickers.selectedStickerId = el.id
+}
+
+function updateSelectedImg(elImg) {
+    gMeme.selectedImgId = elImg.id
+}
+
+function editTxt(txt) {
+    gMeme.txts[gMeme.selectedTxtIdx].line = txt
+}
+function editTxtSize(operator) {
+    if (gMeme.txts[gMeme.selectedTxtIdx].size < 3 && operator < 0) return
+    gMeme.txts[gMeme.selectedTxtIdx].size += operator
+    gMeme.txts[gMeme.selectedTxtIdx].y += operator
+}
+function moveLine(operator) {
+    gMeme.txts[gMeme.selectedTxtIdx].y += operator
+}
+function editTxtAlign(txtAlign) {
+    gMeme.txts[gMeme.selectedTxtIdx].align = txtAlign
+
+}
+function editTxtColor(color) {
+    gMeme.txts[gMeme.selectedTxtIdx].frameColor = color
+}
+function editTxtFillColor(color) {
+    gMeme.txts[gMeme.selectedTxtIdx].color = color
+}
+function changeFont(font) {
+    gMeme.txts[gMeme.selectedTxtIdx].font = font
+}
+function removeTxt() {
+    gMeme.txts.splice(gMeme.selectedTxtIdx, 1)
 
 
-} 
+}
 function addTxt() {
-
-gMeme.txts.push({
-        line: 'I never eat Falafel', 
-        size: 20, 
-        align: 'left', 
-        color: 'red'   
+    var y = 28
+    if (gMeme.txts.length === 1) y = gCanvas.height - y
+    else if (gMeme.txts.length > 1) y = gCanvas.height / 2
+    gMeme.txts.push({
+        line: 'I never eat Falafel',
+        size: 28,
+        align: 'center',
+        color: 'white',
+        frameColor: 'black',
+        font: 'Impact',
+        y,
     })
-    gMeme.selectedTxtIdx++
-}   
-function changeCurrTxt(){
 
+    gMeme.selectedTxtIdx = gMeme.txts.length - 1
+}
+function changeCurrTxt() {
+
+    if (gMeme.selectedTxtIdx >= gMeme.txts.length - 1) {
+        gMeme.selectedTxtIdx = 0
+    } else gMeme.selectedTxtIdx++
 }      
